f4089844c69cdc644cd5df79713a96a51d4be98b
Narun Kitirattragarn <Pun@Puns-MacBook-Pro-4.local>
2014-09-09 13:34:57 +0700

    Initial Commit

---
+++ MyFristProduct/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MyFristProduct
+//
+//  Created by Narun Kitirattragarn on 9/9/14.
+//  Copyright (c) 2014 pun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MyFristProduct/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MyFristProduct
+//
+//  Created by Narun Kitirattragarn on 9/9/14.
+//  Copyright (c) 2014 pun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
